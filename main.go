9eccd28cb8d3508db544902d5d067fb602133f17
Creatorjason <107406861+Creatorjason@users.noreply.github.com>
2023-07-29 08:41:38 +0100

    feat: made some minor changes, if port 9002 is not opened for dev, use default 8080

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 	btc "github.com/ayowilfred95/qoinpal_crypto/bitcoin"
 	ap "github.com/ayowilfred95/qoinpal_crypto/api"
 	"github.com/gin-gonic/gin"
+
 )
 
 func main(){
+	// got
 	engine := gin.Default()
 	fmt.Println("Multichain")
 	fmt.Println(btc.NewBitcoinDisposableWallet())
